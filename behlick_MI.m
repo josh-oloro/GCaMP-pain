c92ef00b5a23413dd82eb220037116e512fb1e0f
Jeric Briones <jeric.briones@gmail.com>
2021-02-08 16:31:02 +0900

    Update behlick_MI.m

--- behlick_MI.m
+++ behlick_MI.m
@@ -14,16 +14,11 @@ for data_i = 1:numel(data)
     conv_x = 60;
     tkmarkers = [1:size(mean_adj1_ROI1, 1)] - start_mark;
     tkmarker_mins = tkmarkers./fps/conv_x;
-    inj_start_min = (inj_start-start_mark)./fps/conv_x;
-    inj_end_min = (inj_end-start_mark)./fps/conv_x;
-    pl_mean1 = plot(tkmarker_mins, mean_adj1_ROI1, 'LineWidth', 3);
-    clc
-    clearvars -except tkmarker_mins mean_adj1_ROI1 BehTime Formalin PBS...
-        data lick_i f data_i
-    close all
     
-    t_roi = tkmarker_mins;
-    roi = mean_adj1_ROI1;
+    all_ROI = [mean_adj1, mean_adj2];
+    
+    clearvars -except tkmarker_mins BehTime Formalin PBS...
+        data lick_i f data_i all_ROI
     
     if data_i <= 4
         lick = Formalin(:,lick_i(data_i));
@@ -31,17 +26,24 @@ for data_i = 1:numel(data)
         lick = PBS(:,lick_i(data_i));
     end
     
+    t_roi = tkmarker_mins;
     t_lick = BehTime + 2.5;
-    
     roi_bin = zeros(size(t_lick));
-    for ti = 1:size(t_lick, 1)
-        st = t_lick(ti);
-        try
-            et = t_lick(ti+1);
-        catch
-            et = t_lick(ti)+2.5;
+    
+    for ri = 1:size(all_ROI, 1);
+        roi = all_ROI(ri,:);
+        
+        for ti = 1:size(t_lick, 1)
+            st = t_lick(ti);
+            try
+                et = t_lick(ti+1);
+            catch
+                et = t_lick(ti)+2.5;
+            end
+            roi_bin(ti) = mean(roi( t_roi >= st & t_roi <= et));
         end
-        roi_bin(ti) = mean(roi( t_roi >= st & t_roi <= et));
+        [f(ri, data_i), ~] = discrete_continuous_info_fast(lick, roi_bin);
     end
-    [f(data_i, :), ~] = discrete_continuous_info_fast(lick, roi_bin);
 end
+
+plot(f, 'o')
